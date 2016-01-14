5e92e545d75ffe1c2bac6b629d75b3108b1d578a
Furkan Kandemir <frknkandemir@gmail.com>
2016-01-14 12:07:58 +0200

    Update post.php

--- post.php
+++ post.php
@@ -1,8 +1,8 @@
 <?php
 	if($_POST){
 		/*	Mysql Veritabanı Bağlantısı */
-		$connx = mysql_connect("localhost","kullanıcı-adi","sifre") or die(mysql_error());
-		$dbConnx = mysql_select_db("veritabanı-adi", $connx) or die(mysql_error());
+		$connx = mysql_connect("localhost","kullanici-adi","sifre") or die(mysql_error());
+		$dbConnx = mysql_select_db("veritabani-adi", $connx) or die(mysql_error());
 		/*	Klavyeden girilen veriyi Alma ve işlemleri yapma */
 		$value = mysql_real_escape_string(strip_tags(rtrim($_POST['value'])));
 		if(!$value){
